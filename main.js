4c8dd346b6fe4516fa75892b6d31fedf1f336c04
barbarad91 <barbarad91dev@gmail.com>
2024-11-29 13:36:24 +0100

    Compute

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-// commit A
-// commit B
-// commit C
-// commit D
-// commit E
+import { sum } from "./sum.js"
+import { subtract } from "./subtract.js"
+
+function compute(a,b){
+    return {
+        sum: sum(a,b),
+        subtract: subtract(a,b)
+    }
+}
