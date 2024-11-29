c1c2ae41da2053bbee73b55847515a7f02f0cd60
barbarad91 <barbarad91dev@gmail.com>
2024-11-29 13:36:50 +0100

    Validate input are numbers

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import { sum } from "./sum.js"
 import { subtract } from "./subtract.js"
 
 function compute(a,b){
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'Please provide two numbers'
+    }
     return {
         sum: sum(a,b),
         subtract: subtract(a,b)
